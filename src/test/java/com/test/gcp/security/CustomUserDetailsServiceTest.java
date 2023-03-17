5117b70eff6a211439f471c5a9847e27e3be4784
avi3s <avirup.pal@gmail.com>
2023-03-18 00:10:14 +0530

    More test class created

---
+++ src/test/java/com/test/gcp/security/CustomUserDetailsServiceTest.java
@@ -0,0 +1,19 @@
+package com.test.gcp.security;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CustomUserDetailsServiceTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/test/gcp/util/EmployeeValidationTest.java
@@ -0,0 +1,19 @@
+package com.test.gcp.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmployeeValidationTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
