bc0f036f267d99f411a3a1097428c383e3fc9f90
avi3s <avirup.pal@gmail.com>
2023-03-20 16:20:52 +0530

    Auth Test Added By Pavani

--- src/main/java/com/test/gcp/service/impl/DepartmentServiceImpl.java
+++ src/main/java/com/test/gcp/service/impl/DepartmentServiceImpl.java
@@ -17,70 +17,74 @@ import com.test.gcp.util.DepartmentValidation;
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
 
-    private static final Logger logger = LogManager.getLogger(DepartmentServiceImpl.class);
+  private static final Logger logger = LogManager.getLogger(DepartmentServiceImpl.class);
 
-    @Autowired
-    private DepartmentValidation departmentValidation;
+  @Autowired
+  private DepartmentValidation departmentValidation;
 
-    @Override
-    public DepartmentDTO createDepartment(final DepartmentDTO departmentDTO) {
+  @Override
+  public DepartmentDTO createDepartment(final DepartmentDTO departmentDTO) {
 
-        logger.log(Level.INFO, () -> "createDepartment method starts ===>>> " + departmentDTO);
+    logger.log(Level.INFO, () -> "createDepartment method starts ===>>> " + departmentDTO);
 
-        departmentValidation.validateAddDepartment(departmentDTO);
-        String departmentId = String.valueOf(DEPARTMENTS.size() + 1);
-        departmentDTO.setDepartmentId(departmentId);
-        DEPARTMENTS.add(departmentDTO);
+    departmentValidation.validateAddDepartment(departmentDTO);
+    String departmentId = String.valueOf(DEPARTMENTS.size() + 1);
+    departmentDTO.setDepartmentId(departmentId);
+    DEPARTMENTS.add(departmentDTO);
 
-        logger.log(Level.INFO, () -> "createDepartment method ends ===>>> " + departmentDTO);
-        return departmentDTO;
+    logger.log(Level.INFO, () -> "createDepartment method ends ===>>> " + departmentDTO);
+    return departmentDTO;
+  }
+
+  @Override
+  public List<DepartmentDTO> getDepartments() {
+    logger.log(Level.INFO, () -> "getDepartments method");
+    return DEPARTMENTS;
+  }
+
+  @Override
+  public DepartmentDTO getDepartmentsById(final String departmentId) {
+
+    logger.log(Level.INFO, () -> "getDepartmentsById method starts ===>>> " + departmentId);
+    DepartmentDTO presentDepartmentDTO = null;
+    Optional<DepartmentDTO> departmentDTO = DEPARTMENTS.stream()
+        .filter(e -> e.getDepartmentId().equals(departmentId)).findFirst();
+    if (departmentDTO.isPresent()) {
+      presentDepartmentDTO = departmentDTO.get();
+    } else {
+      throw new ResourceNotFoundException("departmentId", departmentId);
     }
+    logger.log(Level.INFO, "getDepartmentsById method ends ===>>> " + presentDepartmentDTO);
+    return presentDepartmentDTO;
+  }
 
-    @Override
-    public List<DepartmentDTO> getDepartments() {
-        logger.log(Level.INFO, () -> "getDepartments method");
-        return DEPARTMENTS;
+  @Override
+  public DepartmentDTO updateDepartment(final String departmentId,
+      final DepartmentDTO departmentDTO) {
+
+    logger.log(Level.INFO, () -> "updateDepartment method starts ===>>> " + departmentId);
+
+    DepartmentDTO departmentDTO2 = departmentValidation.validateUpdateDepartment(departmentDTO,
+        departmentId);
+    DEPARTMENTS.remove(departmentDTO2);
+    departmentDTO.setDepartmentId(departmentId);
+    DEPARTMENTS.add(departmentDTO);
+    logger.log(Level.INFO, () -> "updateDepartment method ends ===>>> " + DEPARTMENTS);
+    return departmentDTO;
+  }
+
+  @Override
+  public void deleteDepartment(final String departmentId) {
+
+    logger.log(Level.INFO, () -> "deleteDepartment method starts ===>>> " + departmentId);
+
+    Optional<DepartmentDTO> departmentDTO = DEPARTMENTS.stream()
+        .filter(e -> e.getDepartmentId().equals(departmentId)).findFirst();
+    if (departmentDTO.isPresent()) {
+      DEPARTMENTS.remove(departmentDTO.get());
+    } else {
+      throw new ResourceNotFoundException("departmentId", departmentId);
     }
-
-    @Override
-    public DepartmentDTO getDepartmentsById(final String departmentId) {
-
-        logger.log(Level.INFO, () -> "getDepartmentsById method starts ===>>> " + departmentId);
-        DepartmentDTO presentDepartmentDTO = null;
-        Optional<DepartmentDTO> departmentDTO = DEPARTMENTS.stream().filter(e -> e.getDepartmentId().equals(departmentId)).findFirst();
-        if (departmentDTO.isPresent()) {
-            presentDepartmentDTO = departmentDTO.get();
-        } else {
-            throw new ResourceNotFoundException("departmentId", departmentId);
-        }
-        logger.log(Level.INFO, "getDepartmentsById method ends ===>>> " + presentDepartmentDTO);
-        return presentDepartmentDTO;
-    }
-
-    @Override
-    public DepartmentDTO updateDepartment(final String departmentId, final DepartmentDTO departmentDTO) {
-
-        logger.log(Level.INFO, () -> "updateDepartment method starts ===>>> " + departmentId);
-
-        DepartmentDTO departmentDTO2 = departmentValidation.validateUpdateDepartment(departmentDTO, departmentId);
-        DEPARTMENTS.remove(departmentDTO2);
-        departmentDTO.setDepartmentId(departmentId);
-        DEPARTMENTS.add(departmentDTO);
-        logger.log(Level.INFO, () -> "updateDepartment method ends ===>>> " + DEPARTMENTS);
-        return departmentDTO;
-    }
-
-    @Override
-    public void deleteDepartment(final String departmentId) {
-
-        logger.log(Level.INFO, () -> "deleteDepartment method starts ===>>> " + departmentId);
-
-        Optional<DepartmentDTO> departmentDTO = DEPARTMENTS.stream().filter(e -> e.getDepartmentId().equals(departmentId)).findFirst();
-        if (departmentDTO.isPresent()) {
-            DEPARTMENTS.remove(departmentDTO.get());
-        } else {
-            throw new ResourceNotFoundException("departmentId", departmentId);
-        }
-        logger.log(Level.INFO, () -> "deleteDepartment method ends ===>>> " + DEPARTMENTS);
-    }
+    logger.log(Level.INFO, () -> "deleteDepartment method ends ===>>> " + DEPARTMENTS);
+  }
 }
--- src/test/java/com/test/gcp/controller/AuthControllerTest.java
+++ src/test/java/com/test/gcp/controller/AuthControllerTest.java
@@ -1,26 +1,39 @@
 package com.test.gcp.controller;
 
+import static org.hamcrest.CoreMatchers.any;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.test.gcp.exception.ResourceFoundException;
 import com.test.gcp.payload.AdminDTO;
@@ -28,68 +41,175 @@ import com.test.gcp.payload.JWTAuthResponse;
 import com.test.gcp.payload.LoginDTO;
 import com.test.gcp.payload.Role;
 import com.test.gcp.security.JwtTokenProvider;
+import com.test.gcp.service.EmployeeService;
 
-@ExtendWith(MockitoExtension.class) @TestMethodOrder(OrderAnnotation.class)
+@ExtendWith(MockitoExtension.class) 
+@TestMethodOrder(OrderAnnotation.class) 
+@WebMvcTest(value = AuthController.class) 
+@AutoConfigureMockMvc(addFilters = false)
 class AuthControllerTest {
 
-    @Mock
-    private AuthenticationManager authenticationManager;
+  @MockBean
+  private AuthenticationManager authenticationManager;
 
-    @Mock
-    private PasswordEncoder passwordEncoder;
+  @MockBean
+  private PasswordEncoder passwordEncoder;
 
-    @Mock
-    private JwtTokenProvider tokenProvider;
+  @MockBean
+  private JwtTokenProvider tokenProvider;
 
-    @InjectMocks
-    private AuthController authController;
+  @InjectMocks
+  private AuthController authController;
 
-    @BeforeEach
-    void setUp() throws Exception {
-    }
+  @Autowired
+  private MockMvc mvc;
 
-    @Test
-    void testLoginAdmin() {
-        LoginDTO loginDto = new LoginDTO();
-        loginDto.setPassword("password");
-        loginDto.setEmail("testuser@test.com");
+  @BeforeEach
+  void setUp() throws Exception {
+  }
 
-        Authentication authentication = mock(Authentication.class);
-        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
+//  @Test
+//  void testLoginAdmin() {
+//    LoginDTO loginDto = new LoginDTO();
+//    loginDto.setPassword("password");
+//    loginDto.setEmail("testuser@test.com");
+//
+//    Authentication authentication = mock(Authentication.class);
+//    when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
+//
+//    when(tokenProvider.generateToken(authentication)).thenReturn("test-token");
+//
+//    ResponseEntity<JWTAuthResponse> response = authController.loginAdmin(loginDto);
+//
+//    assertEquals("test-token", response.getBody().getAccessToken());
+//    assertEquals(HttpStatus.OK, response.getStatusCode());
+//  }
 
-        when(tokenProvider.generateToken(authentication)).thenReturn("test-token");
+  @Test @Order(1)
+  void testRegisterAdmin() {
+    AdminDTO adminDTO = new AdminDTO();
+    adminDTO.setEmail("testuser@test.com");
+    adminDTO.setName("Test User");
+    adminDTO.setPassword("password");
+    adminDTO.setAddress("testaddress");
+    Role role = new Role();
+    role.setName("ROLE_ADMIN");
+    when(passwordEncoder.encode(adminDTO.getPassword())).thenReturn("hashed-password");
+    ResponseEntity<String> response = authController.registerAdmin(adminDTO);
+    assertEquals("Admin registered successfully", response.getBody());
+    assertEquals(HttpStatus.CREATED, response.getStatusCode());
+  }
 
-        ResponseEntity<JWTAuthResponse> response = authController.loginAdmin(loginDto);
+  @Test @Order(2)
+  void testRegisterAdmin_ResourceFoundException() {
+    AdminDTO adminDTO = new AdminDTO();
+    adminDTO.setEmail("testuser@test.com");
+    adminDTO.setName("Test User");
+    adminDTO.setPassword("password");
+    adminDTO.setAddress("testaddress");
+    Role role = new Role();
+    role.setName("ROLE_ADMIN");
+    Throwable throwable = assertThrows(ResourceFoundException.class,
+        () -> authController.registerAdmin(adminDTO));
+    assertEquals("Email already found with : 'testuser@test.com'", throwable.getMessage());
+  }
 
-        assertEquals("test-token", response.getBody().getAccessToken());
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-    }
+  // Negative Scenarios
+  @Test @DisplayName("Testing for successful User and Admin registration") @Order(3)
+  void testSuccessAdmin() throws Exception {
 
-    @Test @Order(1)
-    void testRegisterAdmin() {
-        AdminDTO adminDTO = new AdminDTO();
-        adminDTO.setEmail("testuser@test.com");
-        adminDTO.setName("Test User");
-        adminDTO.setPassword("password");
-        adminDTO.setAddress("testaddress");
-        Role role = new Role();
-        role.setName("ROLE_ADMIN");
-        when(passwordEncoder.encode(adminDTO.getPassword())).thenReturn("hashed-password");
-        ResponseEntity<String> response = authController.registerAdmin(adminDTO);
-        assertEquals("Admin registered successfully", response.getBody());
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-    }
+    String input = "{\r\n  \"name\": \"Pavani\",\r\n  \"password\": \"Pavani20@\",\r\n  \"email\": \"k.pavani@gmail.com\",\r\n  \"address\": \"Andhra Pradesh\",\r\n  \"roles\": [\r\n    {\r\n      \"name\": \"ROLE_ADMIN\"\r\n    }\r\n  ]\r\n}";
+    MvcResult result = mvc
+        .perform(MockMvcRequestBuilders.post("/api/auth/signup").content(input)
+            .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+        .andReturn();
 
-    @Test @Order(2)
-    void testRegisterAdmin_ResourceFoundException() {
-        AdminDTO adminDTO = new AdminDTO();
-        adminDTO.setEmail("testuser@test.com");
-        adminDTO.setName("Test User");
-        adminDTO.setPassword("password");
-        adminDTO.setAddress("testaddress");
-        Role role = new Role();
-        role.setName("ROLE_ADMIN");
-        Throwable throwable = assertThrows(ResourceFoundException.class, () -> authController.registerAdmin(adminDTO));
-        assertEquals("Email already found with : 'testuser@test.com'", throwable.getMessage());
-    }
+    System.out.println(result.getResponse().getContentAsString());
+    String expected = "Admin registered successfully";
+    Assertions.assertEquals(201, result.getResponse().getStatus());
+    Assertions.assertEquals(expected, result.getResponse().getContentAsString());
+  }
+
+  @Test @DisplayName("Testing for All Fields Blank") @Order(4)
+  void testAllFieldsBlack() throws Exception {
+
+    String input = "{\r\n  \"name\": \"\",\r\n  \"password\": \"\",\r\n  \"email\": \"\",\r\n  \"address\": \"\",\r\n  \"roles\": [\r\n    {\r\n      \"name\": \"\"\r\n    }\r\n  ]\r\n}";
+    MvcResult result = mvc
+        .perform(MockMvcRequestBuilders.post("/api/auth/signup").content(input)
+            .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+        .andReturn();
+
+    System.out.println(result.getResponse());
+    Assertions.assertEquals(400, result.getResponse().getStatus());
+    assertTrue(
+        result.getResponse().getContentAsString().contains("Name should not be null or empty"));
+    assertTrue(
+        result.getResponse().getContentAsString().contains("Email should not be null or empty"));
+    assertTrue(
+        result.getResponse().getContentAsString().contains("Password should not be null or empty"));
+    assertTrue(result.getResponse().getContentAsString()
+        .contains("Address must be between 10 to 50 characters"));
+    assertTrue(result.getResponse().getContentAsString()
+        .contains("Role Name should not be null or empty"));
+    assertTrue(result.getResponse().getContentAsString()
+        .contains("Role Name should be ROLE_ADMIN or ROLE_USER"));
+
+  }
+
+  @Test @DisplayName("Testing for existing mail") @Order(5)
+  void testExistingMail() throws Exception {
+    AdminDTO signUpDto = new AdminDTO();
+    signUpDto.setName("Pavani");
+    signUpDto.setPassword("Pavani");
+    signUpDto.setEmail("k.pavani@gmail.com");
+    signUpDto.setAddress("Andhra Pradesh");
+    Set<Role> role = new HashSet<>();
+    Role myRole = new Role();
+    myRole.setName("ROLE_ADMIN");
+    role.add(myRole);
+    signUpDto.setRoles(role);
+    EmployeeService.ADMINS.add(signUpDto);
+
+    String input = "{\r\n  \"name\": \"Pavani\",\r\n  \"password\": \"Pavani20@\",\r\n  \"email\": \"k.pavani@gmail.com\",\r\n  \"address\": \"Andhra Pradesh\",\r\n  \"roles\": [\r\n    {\r\n      \"name\": \"ROLE_ADMIN\"\r\n    }\r\n  ]\r\n}";
+    MvcResult result = mvc
+        .perform(MockMvcRequestBuilders.post("/api/auth/signup").content(input)
+            .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+        .andReturn();
+
+    System.out.println(result.getResponse().getContentAsString());
+    Assertions.assertEquals(409, result.getResponse().getStatus());
+    assertTrue(result.getResponse().getContentAsString().contains("Email already found"));
+  }
+
+  @Test @DisplayName("Testing for successful Signin") @Order(6)
+  void testSuccessSignIn() throws Exception {
+
+    String input = "{\r\n  \"password\": \"pavani20@\",\r\n  \"email\": \"k.pavani@gmail.com\"\r\n}";
+    MvcResult result = mvc
+        .perform(MockMvcRequestBuilders.post("/api/auth/signin").content(input)
+            .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+        .andReturn();
+
+    System.out.println(result.getResponse().getContentAsString());
+    Assertions.assertEquals(200, result.getResponse().getStatus());
+    assertTrue(result.getResponse().getContentAsString().contains("Bearer"));
+  }
+
+  @Test @DisplayName("Testing for empty fields Signin") @Order(7)
+  void testEmptySignIn() throws Exception {
+
+    String input = "{\r\n  \"password\": \"\",\r\n  \"email\": \"\"\r\n}";
+    MvcResult result = mvc
+        .perform(MockMvcRequestBuilders.post("/api/auth/signin").content(input)
+            .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+        .andReturn();
+
+    System.out.println(result.getResponse().getContentAsString());
+    Assertions.assertEquals(400, result.getResponse().getStatus());
+    assertTrue(
+        result.getResponse().getContentAsString().contains("Email should not be null or empty"));
+    assertTrue(
+        result.getResponse().getContentAsString().contains("Password should not be null or empty"));
+
+  }
 }
